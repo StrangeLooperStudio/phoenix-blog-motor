7daa4fdf96ad75a828eb4e2122284304fb19041f
Matt Marcum <matt@strangelooper.com>
2018-03-11 22:50:16 -0500

    create user resource

---
+++ lib/phoenix_blog_motor_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixBlogMotorWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use PhoenixBlogMotorWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(PhoenixBlogMotorWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(PhoenixBlogMotorWeb.ErrorView, :"404")
+  end
+end
